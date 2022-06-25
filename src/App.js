bc459c35d9527daca4cfc6235a2a8f2418d9695f
Asrat Minase <asrat461@revature.net>
2022-06-25 17:46:29 -0500

    Resolving Conflict

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
-<<<<<<< HEAD
-//import { createContext, useState } from "react";
-//import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React from "react";
-//import Userprofile from "./components/userprofile/Userprofile";
-import Avatar from "./components/userprofile/Avatar";
-import Avatar2 from "./components/userprofile/Avatar2";
-=======
 
+import { Avatar } from "@mui/material";
 import { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AvatarProfile from "./components/userprofile/profile-avator";
 import ProfileDashboard from "./components/userprofile/profile-dashboard";
 import Welcome from "./components/welcome/welcomepage";
 
->>>>>>> 725b1f89bff00ce70f525676e4b2e7f61257f695
 
-// export const userContext = createContext();
+export const userContext = createContext();
 
 function App() {
 
-  // const [user, setUser] = useState({username: "Guest" })
+  const [user, setUser] = useState({username: "Guest" })
   return (
     <>
-    {/* <BrowserRouter>
+    <BrowserRouter>
          <userContext.Provider value={[user, setUser]}>
              <Routes>
-<<<<<<< HEAD
-                <Route path="" element={< Userprofile/>} />
-                <Route path="/" element={< Avatar/>} />
-=======
+              
                 <Route path="" element={<Welcome />} />
                 <Route path="/" element={<Welcome />} />
                 <Route path="/profiledashboard" element={<ProfileDashboard/>} />
                 <Route path="/profile" element={<AvatarProfile/>}/>
->>>>>>> 725b1f89bff00ce70f525676e4b2e7f61257f695
+              
+                <Route path="/" element={< Avatar/>} />
+                
               </Routes>
          </userContext.Provider>
-     </BrowserRouter> */}
-     <div>
-      <Avatar src="" alt="" />
-      </div>
-      <div>
-      <Avatar2 src="" alt="" />
-      </div>
+     </BrowserRouter>
+
     </>
   );
 }
