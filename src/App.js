96c149c034997e299257e7ece851ef3a06d92439
RF <rfig2k17@gmail.com>
2022-06-27 08:06:29 -0700

    darkmode functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Welcome from "./components/welcome/welcomepage";
+import WelcomeNavBar from "./components/welcome/welcomepagenavbar";
+
 
 export const userContext = createContext();
 
