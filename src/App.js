f3fc271836d9387f9ea426dc3f29c6591e05f52d
HungFai Koo <jhk888@yahoo.com>
2022-06-29 08:09:35 -0700

    just add path Addjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
 import { Avatar } from "@mui/material";
 import { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import ResetPassword from "./components/resetpassword/resetpassword";
-
 
 import AvatarProfile from "./components/userprofile/profile-avator";
 import ProfileDashboard from "./components/userprofile/profile-dashboard";
@@ -12,30 +10,29 @@ import Welcome from "./components/welcome/welcomepage";
 import CustomerLogin from "./components/login/login";
 import { QrCode } from "./components/twofactor/qrCode";
 import { QrLogin } from "./components/twofactor/qrLogin";
-
+import UserRegistration from "./components/register/userregistration";
 
 export const userContext = createContext();
 
 function App() {
-
-  const [user, setUser] = useState({email: "test@test.com" })
+  const [user, setUser] = useState({ email: "test@test.com" });
   return (
     <>
-    <BrowserRouter>
-         <userContext.Provider value={[user, setUser]}>
-             <Routes>
-                <Route path="/profiledashboard" element={<ProfileDashboard/>} />
-                <Route path="/profile" element={<AvatarProfile/>}/>
-                <Route path="/" element={< Welcome/>} />
-                <Route path="login" element={<CustomerLogin />} />
-                <Route path="" element={<Welcome />} />
-                <Route path="/resetpassword" element={<ResetPassword />} />
-                <Route path="/registerqrcode" element={<QrCode />} />
-                <Route path="/loginqrcode" element={<QrLogin />} />
-              </Routes>
-         </userContext.Provider>
-     </BrowserRouter>
-
+      <BrowserRouter>
+        <userContext.Provider value={[user, setUser]}>
+          <Routes>
+            <Route path="/profiledashboard" element={<ProfileDashboard />} />
+            <Route path="/profile" element={<AvatarProfile />} />
+            <Route path="/" element={<Welcome />} />
+            <Route path="login" element={<CustomerLogin />} />
+            <Route path="" element={<Welcome />} />
+            <Route path="/resetpassword" element={<ResetPassword />} />
+            <Route path="/registerqrcode" element={<QrCode />} />
+            <Route path="/loginqrcode" element={<QrLogin />} />
+            <Route path="/register" element={<UserRegistration />} />
+          </Routes>
+        </userContext.Provider>
+      </BrowserRouter>
     </>
   );
 }
