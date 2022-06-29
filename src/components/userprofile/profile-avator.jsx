b39664e4cd73382a4136572a8827d2b6d9c6cec6
Catherine Claunch <catherine490@revature.net>
2022-06-29 17:29:12 -0500

    Edited welcome page

--- src/components/userprofile/profile-avator.jsx
+++ src/components/userprofile/profile-avator.jsx
@@ -1,108 +1,108 @@
 
-import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faYoutube,
-    faFacebook,
-    faTwitter,
-    faInstagram
-  } from "@fortawesome/free-brands-svg-icons";
+// import { useEffect, useState } from "react";
+// import { Button } from "@mui/material";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//     faYoutube,
+//     faFacebook,
+//     faTwitter,
+//     faInstagram
+//   } from "@fortawesome/free-brands-svg-icons";
 
-export default function AvatorProfile() {
+// export default function AvatorProfile() {
 
-    const [profileBody, setProfileBody] = useState("");
-    const [profileB, setProfileB] = useState("");
+//     const [profileBody, setProfileBody] = useState("");
+//     const [profileB, setProfileB] = useState("");
 
-    const url = "http://localhost:9004";
+//     const url = "http://localhost:9004";
 
-    useEffect(() => {
-        profile();
-    }, []);
+//     useEffect(() => {
+//         profile();
+//     }, []);
 
-    async function profile() {
-        try {
+//     async function profile() {
+//         try {
             
-            const response = (await fetch(`${url}/profile/findAllProfile`));
-            const profileResponse = await response.json();
+//             const response = (await fetch(`${url}/profile/findAllProfile`));
+//             const profileResponse = await response.json();
 
-            setProfileBody(profileResponse[0]);
-            setProfileB(profileResponse[0].email)
+//             setProfileBody(profileResponse[0]);
+//             setProfileB(profileResponse[0].email)
 
-        } catch (error) {
-            // console.error("here is the problem ");
-        }
+//         } catch (error) {
+//             // console.error("here is the problem ");
+//         }
         
-    }
+//     }
 
-    return (
-        <>
-            <br></br>
-            <center>
-            <h4>Wlecome to Profile dashboard</h4>
-            <br></br>
-            <br></br>
-            <label >First Name : </label>
-            <output> {profileBody.fname} </output>
-            <br></br>
-            <br></br>
+//     return (
+//         <>
+//             <br></br>
+//             <center>
+//             <h4>Wlecome to Profile dashboard</h4>
+//             <br></br>
+//             <br></br>
+//             <label >First Name : </label>
+//             <output> {profileBody.fname} </output>
+//             <br></br>
+//             <br></br>
    
-            <label >Last Name : </label>
-            <output> {profileBody.lname} </output>
-            <br></br>
-            <br></br>
+//             <label >Last Name : </label>
+//             <output> {profileBody.lname} </output>
+//             <br></br>
+//             <br></br>
        
-            <label >Profile Name : </label>
-            <output> {profileBody.fname}{profileBody.lname} </output>
-            <br></br>
-            <br></br>
+//             <label >Profile Name : </label>
+//             <output> {profileBody.fname}{profileBody.lname} </output>
+//             <br></br>
+//             <br></br>
            
-            <label >Email : </label>
-            <output> {profileB.email} </output>
-            <br></br>
-            <br></br>
+//             <label >Email : </label>
+//             <output> {profileB.email} </output>
+//             <br></br>
+//             <br></br>
         
-            <label >Phone Number : </label>
-            <output> {profileB.phoneNumber} </output>
-            <br></br>
-            <br></br>
+//             <label >Phone Number : </label>
+//             <output> {profileB.phoneNumber} </output>
+//             <br></br>
+//             <br></br>
 
-            <div className="col-75">
-            <label >Country : </label>
-              <select id="country" name="country">
-              <option value="australia">USA</option>
-              <option value="canada">Canada</option>
-              <option value="usa">Australia</option>
-              <option value="usa">Ethiopia</option>
-              </select>
+//             <div className="col-75">
+//             <label >Country : </label>
+//               <select id="country" name="country">
+//               <option value="australia">USA</option>
+//               <option value="canada">Canada</option>
+//               <option value="usa">Australia</option>
+//               <option value="usa">Ethiopia</option>
+//               </select>
 
-            </div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <Button onClick={profile}>Update</Button>
-            <br></br>
-            <br></br>
+//             </div>
+//             <br></br>
+//             <br></br>
+//             <br></br>
+//             <Button onClick={profile}>Update</Button>
+//             <br></br>
+//             <br></br>
 
-            <div className="social-container">
-              <h3>Social Follow</h3>
-              <a href="https://www.youtube.com"
-                className="youtube social">
-                <FontAwesomeIcon icon={faYoutube} size="2x" />
-              </a>
-              <a href="https://www.twitter.com" 
-                className="twitter social">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
-              </a>
-              <a href="https://www.linkedin.com/"
-                 className="instagram social">
-              <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </a>
+//             <div className="social-container">
+//               <h3>Social Follow</h3>
+//               <a href="https://www.youtube.com"
+//                 className="youtube social">
+//                 <FontAwesomeIcon icon={faYoutube} size="2x" />
+//               </a>
+//               <a href="https://www.twitter.com" 
+//                 className="twitter social">
+//                 <FontAwesomeIcon icon={faTwitter} size="2x" />
+//               </a>
+//               <a href="https://www.linkedin.com/"
+//                  className="instagram social">
+//               <FontAwesomeIcon icon={faInstagram} size="2x" />
+//               </a>
 
-             </div>
+//              </div>
   
-            </center>
-        </>
-    );
-}
+//             </center>
+//         </>
+//     );
+// }
 
--- src/components/welcome/welcomepagenavbar.jsx
+++ src/components/welcome/welcomepagenavbar.jsx
@@ -12,40 +12,42 @@ import { Button } from "@mui/material";
 
 export default function WelcomeNavBar(){
 
-    const [darkMode,setDarkMode]= useState(false)
-    const [logoState, setLogoState] = useState(false)
-    const [logoLState, setLogoLState] = useState(true)
+    // const [darkMode,setDarkMode]= useState(false)
+    // const [logoState, setLogoState] = useState(false)
+    // const [logoLState, setLogoLState] = useState(true)
 
-    const darktheme=createTheme({
-      palette: {
-        mode: darkMode? 'dark' : 'light',
-      }
+    // const darktheme=createTheme({
+    //   palette: {
+    //     mode: darkMode? 'dark' : 'light',
+    //   }
       
-    })
+    // })
 
-    function logos(){
+  //   function logos(){
 
-    if(darkMode === true){setLogoState(!logoState); setLogoLState(!logoLState)}
-  else{setLogoState(!logoState); setLogoLState(!logoLState)}}
+  //   if(darkMode === true){setLogoState(!logoState); setLogoLState(!logoLState)}
+  // else{setLogoState(!logoState); setLogoLState(!logoLState)}}
   
     const navigate = useNavigate();
 
     return(
-        <ThemeProvider theme={darktheme}>
-        <Paper style= {{height:"1000px"}} >
+      <>
+        {/* // <ThemeProvider theme={darktheme}>
+        // <Paper style= {{height:"200vh"}}> */}
         
-        <SwitchAppBar check={darkMode} change={()=>{setDarkMode(!darkMode); logos()}}/>
-      {logoState && <img className='logo2' height={80}  src={Logo2} />}
-      {logoLState && <img className='logo' height={80} src={Logo} />}
+        {/* <SwitchAppBar check={darkMode} change={()=>{setDarkMode(!darkMode); logos()}}/> */}
+      {/* {logoState && <img className='logo2' height={80}  src={Logo2} />}
+      {logoLState && <img className='logo' height={80} src={Logo} />} */}
         <nav>
             <center>
-            <h1>Welcome to Overflowing Stacks</h1>
+           
             
-            <Button onClick={() => navigate("/login")}>Login</Button>
-            <Button onClick={() => navigate("/register")}>Register</Button>
+            {/* <Button onClick={() => navigate("/login")}>Login</Button>
+            <Button onClick={() => navigate("/register")}>Register</Button> */}
             </center>
         </nav>
-      </Paper>
-      </ThemeProvider>
+      {/* // </Paper>
+      // </ThemeProvider> */}
+      </>
     )
 }
