77731e85a95d642eeeb2577e8ae71f39a4a5886b
levihoulton <levijhoulton@gmail.com>
2022-06-30 14:25:22 -0500

    fixed some bugs

--- src/components/register/userregistration.jsx
+++ src/components/register/userregistration.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { TextField, OutlinedInput, InputLabel, InputAdornment, FormControl, IconButton, Button, Box, Paper, Card, CardContent, Typography } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import Image from "/Users/rf/Desktop/P3_OverflowingStacks_FE/src/components/login/login_bg.jpeg";
+import Image from "./login_bg.jpeg";
 import { createTheme } from "@mui/material";
 import { userContext } from "../../App";
 
@@ -55,7 +55,7 @@ const [values, setValues] = React.useState({
 
   const navigate = useNavigate();
 
-  const [user,setUser] = useContext(userContext)
+  const [user,setUser] = React.useContext(userContext)
 
   const url = "https://overflowingstacks.azurewebsites.net";
 
@@ -92,137 +92,78 @@ const [values, setValues] = React.useState({
   return (
 
 
-<Paper style={styles.heroContainer}> 
+    <Paper style={styles.heroContainer}> 
+            
+          <center><div className="App">
+          
         
-      <center><div className="App">
-      
     
-
-      <center>
-     <br></br>
-     <br></br>
-     <br></br>
-   <Card check={darkMode} change={()=>{setDarkMode(!darkMode)}} sx={{ boxShadow: 5,
-          borderRadius: 2, width: 400, height: 700 }}>
-      <CardContent>
-     
-     
           <center>
-          <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-          <Typography sx={{  }} color="text.secondary" gutterBottom>
-          Please Register below
-      </Typography>
-          <TextField id="outlined-basic" label="Email" variant="outlined" inputRef={emailInput}/>
-          <TextField id="outlined-basic" label="First Name" variant="outlined" inputRef={fnameInput}/>
-          <TextField id="outlined-basic" label="Last Name" variant="outlined" inputRef={lnameInput}/>
-          <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
-          <TextField id="outlined-basic" label="Username" variant="outlined" />
-          
-          <br></br>
-          <FormControl sx={{ m: 1, width: '28ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-          <br></br>
-
-          <TextField id="outlined-basic" label="Date of Birth" variant="outlined" />
-          
-        </FormControl>
-          <br></br>
-          <br></br>
-          <Button variant='contained' onClick={register}>Register</Button>
-          
-          </Box>
-          </center>
-          </CardContent>
-          </Card>
-          </center>
-          </div>
-          </center>
-          </Paper>
-
-
-
-    // <div className="container">
-    //   <h4>Join OverflowingStacks!!!</h4>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter your email"
-    //     ref={emailInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter first name"
-    //     ref={fnameInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter last name"
-    //     ref={lnameInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter phone number"
-    //     ref={phonenumberInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter username"
-    //     ref={usernameInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     type="password"
-    //     placeholder="Please enter your password"
-    //     ref={passwordInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter your dob"
-    //     ref={dobInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <Button variant="contained" color="primary" onClick={register}>
-    //     Sign Up
-    //   </Button>
-    // </div>
-  );
-}
+         <br></br>
+         <br></br>
+         <br></br>
+       <Card check={darkMode} change={()=>{setDarkMode(!darkMode)}} sx={{ boxShadow: 5,
+              borderRadius: 2, width: 400, height: 700 }}>
+          <CardContent>
+         
+         
+              <center>
+              <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+              <Typography sx={{  }} color="text.secondary" gutterBottom>
+              Please Register below
+          </Typography>
+              <TextField id="outlined-basic" label="Email" variant="outlined" inputRef={emailInput}/>
+              <TextField id="outlined-basic" label="First Name" variant="outlined" inputRef={fnameInput}/>
+              <TextField id="outlined-basic" label="Last Name" variant="outlined" inputRef={lnameInput}/>
+              <TextField id="outlined-basic" label="Phone Number" variant="outlined" inputRef={phonenumberInput}/>
+              <TextField id="outlined-basic" label="Username" variant="outlined" inputRef={usernameInput}/>
+              
+              <br></br>
+              <FormControl sx={{ m: 1, width: '28ch' }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={values.password}
+                onChange={handleChange('password')}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+              <br></br>
+    
+              <TextField id="outlined-basic" label="Date of Birth" variant="outlined" inputRef={dobInput}/>
+              
+            </FormControl>
+              <br></br>
+              <br></br>
+              <Button variant='contained' onClick={register}>Register</Button>
+              
+              </Box>
+              </center>
+              </CardContent>
+              </Card>
+              </center>
+              </div>
+              </center>
+              </Paper>
+    
+      );
+    }
