7262728ed72f94895f8ddbb1aad42faeb96c77ae
JerryFaustin <jerfstn@gmail.com>
2022-06-30 18:42:16 -0400

    Updated conditionals regarding Register

--- src/components/register/userregistration.jsx
+++ src/components/register/userregistration.jsx
@@ -4,6 +4,7 @@ import { useRef, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { userContext } from "../../App";
+import { isValidFormat } from "@firebase/util";
 
 export default function UserRegistration() {
   const navigate = useNavigate();
@@ -21,6 +22,7 @@ export default function UserRegistration() {
   const dobInput = useRef();
 
   async function register() {
+    let valid = true;
     const userprofile = {
       email: emailInput.current.value,
       fname: fnameInput.current.value,
@@ -34,35 +36,60 @@ export default function UserRegistration() {
 
    // let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    
-    if (emailInput.current.value === "" || passwordInput.current.value === "") {
+      try {
+      const response = await fetch(`${url}/users/findAllUsers`);
+      const usersPulled = await response.json();
+      console.log(usersPulled);
+      for (let i = 0; i < usersPulled.length; i++ ){
+      if(usersPulled[i].email === emailInput.current.value){
+        console.log("User Exists");
+        valid = false;
+        alert("User already exists");
+        break;
+      }
+        
+      }
+    }catch(e){
+      console.log(null);
+  }
+     if (emailInput.current.value === "" || passwordInput.current.value === "") {
+      valid = false;
       alert("You need to enter valid email and password");
     }// else if (emailInput.current.value.match(mailformat));
     else if (fnameInput.current.value === "" || lnameInput.current.value === "") {
+      valid = false;
       alert("You need to enter valid First or Last Name");
     }
     else if (usernameInput.current.value === "" || !isNaN(usernameInput.current.value) ) {
+      valid = false;
       alert("You need to enter valid Username");
     }
     else if (phonenumberInput.current.value === "" || isNaN(phonenumberInput.current.value)) {
+      valid = false;
       alert("You need to enter valid Phone Number");
     }
     else if (dobInput.current.value === "") {
+      valid = false;
       alert("You need to enter valid Date of Birth");
     }
     else if(!isNaN(emailInput.current.value)){
+      valid = false;
       alert("You need to enter valid email");
     }
     else if(!isNaN(fnameInput.current.value)){
+      valid = false;
       alert("You need to enter valid first name")
     }
     else if(!isNaN(lnameInput.current.value)){
+      valid = false;
       alert("You need to enter valid last name")
     }
     else if(!isNaN(passwordInput.current.value)){
+      valid = false;
       alert("Your Password needs to include letters")
     }
     else {
+      if(valid === true){
       try {
         const response = await axios.post(`${url}/users/register`, userprofile);
         console.log(response.data);
@@ -75,6 +102,21 @@ export default function UserRegistration() {
 
     }
   }
+  }
+    async function checkUserName(){
+      try {
+        const response = await fetch(`${url}/users/findAllUsers`);
+        const usersPulled = await response.json();
+        console.log(usersPulled);
+        for (let i = 0; i < usersPulled.length; i++ ){
+          
+          console.log(usersPulled[i].email);
+        }
+      }catch(e){
+        console.log(null);
+    }
+  }
+
 
   return (
     <div className="container">
@@ -133,10 +175,14 @@ export default function UserRegistration() {
         ref={dobInput}
       ></input>
       <br></br>
-
+      
       <Button variant="contained" color="primary" onClick={register}>
         Sign Up
+      </Button>
+      <Button variant="contained" color="primary" onClick={checkUserName}>
+        TestGet
       </Button>
     </div>
   );
 }
+
