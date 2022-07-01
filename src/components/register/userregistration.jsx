0395fe89544494ac2b13aeac84767f9a18c629ca
Geoffrey <bloisgeoffrey@yahoo.com>
2022-07-01 15:04:11 -0500

    adding some chenges that got deleted

--- src/components/register/userregistration.jsx
+++ src/components/register/userregistration.jsx
@@ -4,9 +4,10 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { TextField, OutlinedInput, InputLabel, InputAdornment, FormControl, IconButton, Button, Box, Paper, Card, CardContent, Typography } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import Image from "/Users/rf/Desktop/P3_OverflowingStacks_FE/src/components/login/login_bg.jpeg";
+import Image from "./login_bg.jpeg";
 import { createTheme } from "@mui/material";
 import { userContext } from "../../App";
+import { isValidFormat } from "@firebase/util";
 
 const styles = {
   heroContainer: {
@@ -55,7 +56,7 @@ const [values, setValues] = React.useState({
 
   const navigate = useNavigate();
 
-  const [user,setUser] = useContext(userContext)
+  const [user,setUser] = React.useContext(userContext)
 
   const url = "https://overflowingstacks.azurewebsites.net";
 
@@ -68,6 +69,7 @@ const [values, setValues] = React.useState({
   const dobInput = useRef();
 
   async function register() {
+    let valid = true;
     const userprofile = {
       email: emailInput.current.value,
       fname: fnameInput.current.value,
@@ -78,151 +80,170 @@ const [values, setValues] = React.useState({
       dob: dobInput.current.value
     };
 
-    try {
-      const response = await axios.post(`https://overflowingstacks.azurewebsites.net/users/register`, userprofile);
-      console.log(response.data);
-      setUser({...user, email: emailInput.current.value})
-      navigate("/registerqrcode");
-    } catch (error) {
-      console.error(error.response.data);
-      console.log(error);
+
+
+   // let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+      try {
+      const response = await fetch(`${url}/users/findAllUsers`);
+      const usersPulled = await response.json();
+      console.log(usersPulled);
+      for (let i = 0; i < usersPulled.length; i++ ){
+      if(usersPulled[i].email === emailInput.current.value){
+        console.log("User Exists");
+        valid = false;
+        alert("User already exists");
+        break;
+      }
+        
+      }
+    }catch(e){
+      console.log(null);
+  }
+     if (emailInput.current.value === "" || passwordInput.current.value === "") {
+      valid = false;
+      alert("You need to enter valid email and password");
+    }// else if (emailInput.current.value.match(mailformat));
+    else if (fnameInput.current.value === "" || lnameInput.current.value === "") {
+      valid = false;
+      alert("You need to enter valid First or Last Name");
+    }
+    else if (usernameInput.current.value === "" || !isNaN(usernameInput.current.value) ) {
+      valid = false;
+      alert("You need to enter valid Username");
+    }
+    else if (phonenumberInput.current.value === "" || isNaN(phonenumberInput.current.value)) {
+      valid = false;
+      alert("You need to enter valid Phone Number");
+    }
+    else if (dobInput.current.value === "") {
+      valid = false;
+      alert("You need to enter valid Date of Birth");
+
+    }
+    else if(!isNaN(emailInput.current.value)){
+      valid = false;
+      alert("You need to enter valid email");
+    }
+    else if(!isNaN(fnameInput.current.value)){
+      valid = false;
+      alert("You need to enter valid first name")
+    }
+    else if(!isNaN(lnameInput.current.value)){
+      valid = false;
+      alert("You need to enter valid last name")
+    }
+    else if(!isNaN(passwordInput.current.value)){
+      valid = false;
+      alert("Your Password needs to include letters")
+    }
+    else {
+      if(valid === true){
+        setUser({...user, email: emailInput.current.value})
+      try {
+        const response = await axios.post(`${url}/users/register`, userprofile);
+        console.log(response.data)
+        navigate("/registerqrcode");
+      } catch (error) {
+        console.error(error.response.data);
+        console.log(error);
+        alert(error.response.data);
+      }
+
     }
   }
+  }
+    async function checkUserName(){
+      try {
+        const response = await fetch(`${url}/users/findAllUsers`);
+        const usersPulled = await response.json();
+        console.log(usersPulled);
+        for (let i = 0; i < usersPulled.length; i++ ){
+          
+          console.log(usersPulled[i].email);
+        }
+      }catch(e){
+        console.log(null);
+    }
+  }
+
 
   return (
 
 
-<Paper style={styles.heroContainer}> 
+    <Paper style={styles.heroContainer}> 
+            
+          <center><div className="App">
+          
         
-      <center><div className="App">
-      
     
-
-      <center>
-     <br></br>
-     <br></br>
-     <br></br>
-   <Card check={darkMode} change={()=>{setDarkMode(!darkMode)}} sx={{ boxShadow: 5,
-          borderRadius: 2, width: 400, height: 700 }}>
-      <CardContent>
-     
-     
           <center>
-          <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-          <Typography sx={{  }} color="text.secondary" gutterBottom>
-          Please Register below
-      </Typography>
-          <TextField id="outlined-basic" label="Email" variant="outlined" inputRef={emailInput}/>
-          <TextField id="outlined-basic" label="First Name" variant="outlined" inputRef={fnameInput}/>
-          <TextField id="outlined-basic" label="Last Name" variant="outlined" inputRef={lnameInput}/>
-          <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
-          <TextField id="outlined-basic" label="Username" variant="outlined" />
-          
-          <br></br>
-          <FormControl sx={{ m: 1, width: '28ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-          <br></br>
-
-          <TextField id="outlined-basic" label="Date of Birth" variant="outlined" />
-          
-        </FormControl>
-          <br></br>
-          <br></br>
-          <Button variant='contained' onClick={register}>Register</Button>
-          
-          </Box>
-          </center>
-          </CardContent>
-          </Card>
-          </center>
-          </div>
-          </center>
-          </Paper>
-
-
-
-    // <div className="container">
-    //   <h4>Join OverflowingStacks!!!</h4>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter your email"
-    //     ref={emailInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter first name"
-    //     ref={fnameInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter last name"
-    //     ref={lnameInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter phone number"
-    //     ref={phonenumberInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter username"
-    //     ref={usernameInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     type="password"
-    //     placeholder="Please enter your password"
-    //     ref={passwordInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <input
-    //     size="30"
-    //     placeholder="Please enter your dob"
-    //     ref={dobInput}
-    //   ></input>
-    //   <br></br>
-
-    //   <Button variant="contained" color="primary" onClick={register}>
-    //     Sign Up
-    //   </Button>
-    // </div>
-  );
-}
+         <br></br>
+         <br></br>
+         <br></br>
+       <Card check={darkMode} change={()=>{setDarkMode(!darkMode)}} sx={{ boxShadow: 5,
+              borderRadius: 2, width: 400, height: 700 }}>
+          <CardContent>
+         
+         
+              <center>
+              <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+              <Typography sx={{  }} color="text.secondary" gutterBottom>
+              Please Register below
+          </Typography>
+              <TextField id="outlined-basic" label="Email" variant="outlined" inputRef={emailInput}/>
+              <TextField id="outlined-basic" label="First Name" variant="outlined" inputRef={fnameInput}/>
+              <TextField id="outlined-basic" label="Last Name" variant="outlined" inputRef={lnameInput}/>
+              <TextField id="outlined-basic" label="Phone Number" variant="outlined" inputRef={phonenumberInput}/>
+              <TextField id="outlined-basic" label="Username" variant="outlined" inputRef={usernameInput}/>
+              
+              <br></br>
+              <FormControl sx={{ m: 1, width: '28ch' }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                inputRef={passwordInput}
+                value={values.password}
+                onChange={handleChange('password')}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+              <br></br>
+    
+              <TextField id="outlined-basic" type="date" label="Date of Birth" variant="outlined" inputRef={dobInput} InputLabelProps={{ shrink: true}}/>
+              
+            </FormControl>
+              <br></br>
+              <br></br>
+              <Button variant='contained' onClick={register}>Register</Button>
+              
+              </Box>
+              </center>
+              </CardContent>
+              </Card>
+              </center>
+              </div>
+              </center>
+              </Paper>
+    
+      );
+    }
