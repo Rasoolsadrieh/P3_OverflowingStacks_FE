b3d3128acd1887f29faac6dc555d5581b12b983a
Ay Sebirka <ay914@revature.net>
2022-07-01 11:54:05 -0500

    fixing the URL

--- src/components/sendmoney/transfermoney.jsx
+++ src/components/sendmoney/transfermoney.jsx
@@ -1,17 +1,18 @@
 import axios from "axios";
-import { useRef, useState } from "react";
+import { useRef, useState, useContext } from "react";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { userContext } from "../../App";
 
 
 export default function Payment() {
   
 
 
-    const url = "https://overflowingstacksweb.azurewebsites.net/";
+    const url = "https://overflowingstacks.azurewebsites.net";
     
     const navigate = useNavigate();
-    //const [user] = useContext(userContext)
+    const [user] = useContext(userContext)
 
 
     const [showPay, setPay] = useState(false);
@@ -19,21 +20,20 @@ export default function Payment() {
     const profileNameInput = useRef();
     const receiverProfileInput =useRef();
     const paymentInput = useRef();
+    
      
     
   
     async function transferMoneyNow() {
 
 
-        const profileResponse = await axios.get(`${url}/findProfile?findProfile=${profileNameInput.current.value}`)
-        const receiverProfileResponse = await axios.get(`${url}/findProfile?findProfile=${receiverProfileInput.current.value}`)
+        const profileResponse = await axios.get(`${url}/profile/findProfile?profileName=${profileNameInput.current.value}`)
+        const receiverProfileResponse = await axios.get(`${url}/profile/findProfile?profileName=${receiverProfileInput.current.value}`)
 
         
-        const user = {
+        const sender = {
 
             profileName: profileResponse.data.profileName,
-            fname: profileResponse.data.fname,
-            lname: profileResponse.data.lname,
             email: profileResponse.data.email,
             balance: profileResponse.data.balance - paymentInput.current.value,
             accountName: profileResponse.data.accountName,
@@ -44,22 +44,20 @@ export default function Payment() {
          const receiver = {
 
             profileName: receiverProfileResponse.data.profileName,
-            fname: receiverProfileResponse.data.fname,
-            lname: receiverProfileResponse.data.lname,
             email: receiverProfileResponse.data.email,
             balance: receiverProfileResponse.data.balance + paymentInput.current.value,
             accountName: receiverProfileResponse.data.accountName,
             accountNumber: receiverProfileResponse.data.accountNumber,
 
-            isRecieved: true,  
+             
 
          };
                 
              
     
         try {
-            const response = await axios.put(`${url}/updateProfile`, user);
-            const response2 = await axios.put(`${url}/updateProfile`, receiver);
+            const response = await axios.put(`${url}/profile/updateProfile`, sender);
+            const response2 = await axios.put(`${url}/profile/updateProfile`, receiver);
             console.log(response.data);
             console.log(response2.data);
             setPay(!showPay)
@@ -77,9 +75,10 @@ export default function Payment() {
         <>
                 
                 <center>
-                <h4>You can pay your balance here here.</h4>
+                <h4>You can pay your balance here.</h4>
+                <h4>{user.email}</h4>
                 <br></br>  
-                <input  placeholder="Enter your Profile Name" ref={profileNameInput}></input>
+                <input  placeholder="Confirm your Profile Name" ref={profileNameInput}></input>
                 <br></br>
                 <input  placeholder="Enter The profile name of the Receiver" ref={receiverProfileInput}></input>
                 <br></br>
@@ -92,7 +91,7 @@ export default function Payment() {
                 <Button  onClick={transferMoneyNow}>Send Money</Button>
                 <br></br>
                 <br></br>
-                <Button  onClick={() => navigate("/profile")}>Back</Button>       
+                <Button  onClick={() => navigate("/profiledashboard")}>Back</Button>       
                 </center>        
         </>
     );
